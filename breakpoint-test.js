5fc0e2f2ac9c7e992facc170b6f7b24054676c1e
Adil-M2004 <D00265739@student.dkit.ie>
2026-04-29 12:24:52 +0100

    added more tests

---
+++ breakpoint-test.js
@@ -0,0 +1,17 @@
+import http from 'k6/http';
+import { sleep } from 'k6';
+
+export const options = {
+
+    stages: [
+        { duration: '2h', target: 10000 }
+    ],
+}
+export default function () {
+    http.get('https://quickpizza.grafana.com/test.k6.io/');
+    sleep(1);
+    http.get('https://quickpizza.grafana.com/contacts.php');
+    sleep(2);
+    http.get('https://quickpizza.grafana.com/news.php');
+    sleep(2);
+}
